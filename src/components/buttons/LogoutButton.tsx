8163de3a93692b872f6157e0acf754277eacb7de
dolphin-pc <pcx474@gmail.com>
2024-05-03 16:04:05 +0900

    next-auth middleware 적용

--- src/components/buttons/LogoutButton.tsx
+++ src/components/buttons/LogoutButton.tsx
@@ -5,13 +5,7 @@ import Button from "./Button";
 import { signOut } from "next-auth/react";
 
 const LogoutButton = ({ children }: { children: string }) => {
-  return (
-    <Button
-      onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })}
-    >
-      {children}
-    </Button>
-  );
+  return <Button onClick={() => signOut()}>{children}</Button>;
 };
 
 export default LogoutButton;
